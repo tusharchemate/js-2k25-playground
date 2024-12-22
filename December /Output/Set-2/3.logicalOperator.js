06587f845569536e7f93316a8de2300d143cc03e
Tushar Chemate <41479583+tusharchemate@users.noreply.github.com>
2024-12-22 16:59:12 +0530

    Create 3.logicalOperator.js

---
+++ December /Output/Set-2/3.logicalOperator.js
@@ -0,0 +1,6 @@
+console.log(1 || 0);    // ? 1
+console.log(0 || 1);    // ? 1
+console.log(1 && 0);    // ? 0
+console.log(0 && 1);    // ? 0
+console.log(null || undefined); // ? undefined
+console.log(null && undefined); // ? null

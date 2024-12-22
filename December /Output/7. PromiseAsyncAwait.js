3c12b07f8a56209e4fac924c733b299becf91a9e
Tushar Chemate <41479583+tusharchemate@users.noreply.github.com>
2024-12-22 16:50:37 +0530

    Create 7. PromiseAsyncAwait.js

---
+++ December /Output/7. PromiseAsyncAwait.js
@@ -0,0 +1,15 @@
+console.log("Start");
+
+setTimeout(() => console.log("Timeout"), 0);
+
+Promise.resolve().then(() => console.log("Promise"));
+
+console.log("End");
+
+/* 
+	o/p
+  start
+  end 
+  Promise
+  Timeout
+*/

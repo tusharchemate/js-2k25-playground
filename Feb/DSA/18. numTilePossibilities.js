de82fff4b9f4bae48e04bbcc578ff9e49ec6c379
Tushar Chemate <41479583+tusharchemate@users.noreply.github.com>
2025-02-17 21:25:14 +0530

    Create 18. numTilePossibilities.js

---
+++ Feb/DSA/18. numTilePossibilities.js
@@ -0,0 +1,17 @@
+var numTilePossibilities = function(tiles) {
+    tiles = tiles.split("").sort().join("");
+    let used = new Array(tiles.length).fill(false);
+    
+    const backtrack = () => {
+        let count = 0;
+        for (let i = 0; i < tiles.length; i++) {
+            if (used[i] || (i > 0 && tiles[i] === tiles[i - 1] && !used[i - 1])) continue;
+            used[i] = true;
+            count += 1 + backtrack();
+            used[i] = false;
+        }
+        return count;
+    };
+
+    return backtrack();
+};

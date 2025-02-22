8066e4af6ebd05c32b067ddcea42c5f0a33c61db
Tushar Chemate <41479583+tusharchemate@users.noreply.github.com>
2025-02-22 15:34:21 +0530

    Create 21. canCompleteCircuit.js

---
+++ Feb/DSA/21. canCompleteCircuit.js
@@ -0,0 +1,15 @@
+var canCompleteCircuit = function (gas, cost) {
+    let total_tank = 0, curr_tank = 0, start_index = 0;
+
+    for (let i = 0; i < gas.length; i++) {
+        total_tank += gas[i] - cost[i];
+        curr_tank += gas[i] - cost[i];
+        curr_tank += gas[i] - cost[i];
+
+        if (curr_tank < 0) {
+            start_index = i + 1;
+            curr_tank = 0;
+        }
+    }
+    return total_tank >= 0 ? start_index : -1;
+};
